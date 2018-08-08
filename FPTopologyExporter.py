29254958bcd9ac05a0f1430b5728a6d5e3480a31
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2018-08-08 15:57:50 +0200

    export widget id also

--- FPTopologyExporter.py
+++ FPTopologyExporter.py
@@ -5,11 +5,11 @@ import json
 
 
 def exportTopology():
-    topology = dict()
+    dimension = dict()
     for obj in FreeCAD.ActiveDocument.Objects:
         # "FPSimLinearPotentiometer", "FPSimButton", "FPSimDisplay", "FPSimRotaryEncoder", "FPSimRotaryPotentiometer", "FPSimLED"
         label = obj.Label
-        dim = (1,1)
+        dim = None
         if obj.Name.find("FPSim") == 0:
             match = re.match("(.*)\[(\d+)\]\[(\d+)\]", obj.Label)
             if match:
@@ -20,11 +20,19 @@ def exportTopology():
                 if match:
                     label = match.group(1)
                     dim = (int(match.group(2)) + 1, 1)
-            if(label in topology):
-                maxDim = (max(dim[0], topology[label][0]), max(dim[1], topology[label][1]))
+
+            if(dim and label in dimension):
+                maxDim = (max(dim[0], dimension[label][0]), max(dim[1], dimension[label][1]))
             else:
                 maxDim = dim
-            topology[label] = maxDim
+            dimension[label] = maxDim
+
+    topology = []
+    id = 0
+    for label in dimension:
+        widget = {'Id' : id, 'Label' : label, 'Dimension' : dimension[label]}
+        topology.append(widget)
+        id = id + 1
 
 
     fileName = QtGui.QFileDialog.getSaveFileName( filter="Json file (*.json)" )[0]
