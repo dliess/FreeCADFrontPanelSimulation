f93c394638783a394fa2c17b8f88957d89837a95
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2018-08-22 23:02:41 +0300

    started adding some Unit Tests

---
+++ ClientHelpers/c++/CallbackStack.h
@@ -0,0 +1,155 @@
+#include <cstdint> // uint8_t
+
+template <class CallbackIf, unsigned int Size>
+class CallbackStack
+{
+public:
+    CallbackStack() :
+        m_cb(),
+        m_topIndex(0),
+        m_botIndex(0)
+    {}
+    void pushBack(CallbackIf* pCbIf)
+    {
+        if(!m_cb[m_topIndex])
+        {
+            incTopIndex();
+        }
+        m_cb[m_topIndex] = pCbIf;
+    }
+    CallbackIf* getActual() const
+    {
+        return m_cb[m_topIndex];
+    }
+    void remove(const CallbackIf* pCbIf)
+    {
+        m_cb[m_topIndex] == pCbIf;
+        m_cb[m_topIndex] = nullptr;
+        for(uint8_t i = decIndex(m_topIndex); i != m_topIndex; i = decIndex(i))
+        {
+            if(m_cb[i] == pCbIf)
+            {
+                for(uint8_t j = i; j != m_topIndex; j = incIndex(j))
+                {
+                    m_cb[j] = m_cb[incIndex(j)];
+                    m_cb[incIndex(j)] = nullptr;
+                }
+                break;
+            }
+        }
+    }
+private:
+    CallbackIf* m_cb[Size];
+    int8_t  m_topIndex;
+    int8_t  m_botIndex;
+    void incTopIndex()
+    {
+        m_topIndex = incIndex(m_topIndex); 
+        if(m_topIndex == m_botIndex)
+        { 
+            m_botIndex = incIndex(m_botIndex);
+        }
+    }
+    uint8_t incIndex(uint8_t index)
+    {
+        ++index;
+        if(index >= Size)
+        {
+            index=0;
+        }
+        return index;
+    }
+    uint8_t decIndex(uint8_t index)
+    {
+        if(index == 0)
+        {
+            index = Size - 1;
+        }
+        else
+        {
+            --index;
+        }
+        return index;
+    }
+};
+
+template <class CallbackIf>
+class CallbackStack<CallbackIf, 0>
+{
+};
+
+template <class CallbackIf>
+class CallbackStack<CallbackIf, 1>
+{
+public:
+    CallbackStack() :
+        m_cb(nullptr)
+    {}
+    void pushBack(CallbackIf* pCbIf)
+    {
+        m_cb = pCbIf;
+    }
+    CallbackIf* getActual() const
+    {
+        return m_cb;
+    }
+    void remove(const CallbackIf* pCbIf)
+    {
+        if(m_cb == pCbIf)
+        {
+            m_cb = nullptr;
+        } 
+    }
+private:
+    CallbackIf* m_cb;
+};
+
+template <class CallbackIf>
+class CallbackStack<CallbackIf, 2>
+{
+public:
+    CallbackStack() :
+        m_cb{nullptr, nullptr}
+    {}
+    void pushBack(CallbackIf* pCbIf)
+    {
+        if(m_cb[1])
+        {
+            m_cb[0] = m_cb[1];
+            m_cb[1] = pCbIf;
+        }
+        else if(m_cb[0])
+        {
+            m_cb[1] = pCbIf;
+        }
+        else
+        {
+            m_cb[0] = pCbIf;
+        }
+    }
+    CallbackIf* getActual() const
+    {
+        if(m_cb[1])
+        {
+            return m_cb[1];
+        }
+        else
+        {
+            return m_cb[0];
+        }
+    }
+    void remove(const CallbackIf* pCbIf)
+    {
+        if(m_cb[1] == pCbIf)
+        {
+            m_cb[1] = nullptr;
+        }
+        if(m_cb[0] == pCbIf)
+        {
+            m_cb[0] = m_cb[1];
+            m_cb[0] = nullptr;
+        }
+    }
+private:
+    CallbackIf* m_cb[2];
+};
---
+++ ClientHelpers/c++/UnitTest/UT_CallbackStack.cpp
@@ -0,0 +1,145 @@
+#include <gtest/gtest.h>
+#include "CallbackStack.h"
+
+class ICallback
+{
+public:
+    virtual void callback_fn() = 0;
+};
+
+class CBImpl : public ICallback
+{
+public:
+    virtual void callback_fn(){};
+};
+ 
+TEST(CallbackStackTest, EmptyStack) {
+    CallbackStack<ICallback, 1> cbStack1;
+    CallbackStack<ICallback, 2> cbStack2;
+    CallbackStack<ICallback, 3> cbStack3;
+    ASSERT_EQ(nullptr, cbStack1.getActual());
+    ASSERT_EQ(nullptr, cbStack2.getActual());
+    ASSERT_EQ(nullptr, cbStack3.getActual());
+}
+
+TEST(CallbackStackTest, FillStack) {
+    CallbackStack<ICallback, 1> cbStack1;
+    CallbackStack<ICallback, 2> cbStack2;
+    CallbackStack<ICallback, 3> cbStack3;
+    CBImpl fun1;
+    CBImpl fun2;
+    CBImpl fun3;
+    CBImpl fun4;
+
+    cbStack1.pushBack(&fun1);
+    ASSERT_EQ(&fun1, cbStack1.getActual());
+    cbStack1.pushBack(&fun2);
+    ASSERT_EQ(&fun2, cbStack1.getActual());
+    cbStack1.pushBack(&fun3);
+    ASSERT_EQ(&fun3, cbStack1.getActual());
+
+    cbStack2.pushBack(&fun1);
+    ASSERT_EQ(&fun1, cbStack2.getActual());
+    cbStack2.pushBack(&fun2);
+    ASSERT_EQ(&fun2, cbStack2.getActual());
+    cbStack2.pushBack(&fun3);
+    ASSERT_EQ(&fun3, cbStack2.getActual());
+
+    cbStack3.pushBack(&fun1);
+    ASSERT_EQ(&fun1, cbStack3.getActual());
+    cbStack3.pushBack(&fun2);
+    ASSERT_EQ(&fun2, cbStack3.getActual());
+    cbStack3.pushBack(&fun3);
+    ASSERT_EQ(&fun3, cbStack3.getActual());
+    cbStack3.pushBack(&fun4);
+    ASSERT_EQ(&fun4, cbStack3.getActual());
+}
+
+TEST(CallbackStackTest, ClearAll) {
+    CallbackStack<ICallback, 1> cbStack1;
+    CallbackStack<ICallback, 2> cbStack2;
+    CallbackStack<ICallback, 3> cbStack3;
+    CBImpl fun1;
+    CBImpl fun2;
+    CBImpl fun3;
+    CBImpl fun4;
+
+    cbStack1.pushBack(&fun1);
+    cbStack1.remove(&fun2);
+    ASSERT_EQ(&fun1, cbStack1.getActual());
+    cbStack1.remove(&fun1);
+    ASSERT_EQ(nullptr, cbStack1.getActual());
+
+    cbStack2.pushBack(&fun1);
+    cbStack2.pushBack(&fun1);
+    cbStack2.remove(&fun2);
+    ASSERT_EQ(&fun1, cbStack2.getActual());
+    cbStack2.remove(&fun1);
+    ASSERT_EQ(nullptr, cbStack2.getActual());
+
+    cbStack3.pushBack(&fun1);
+    cbStack3.pushBack(&fun1);
+    cbStack3.pushBack(&fun1);
+    cbStack3.remove(&fun2);
+    ASSERT_EQ(&fun1, cbStack3.getActual());
+    cbStack3.remove(&fun1);
+    ASSERT_EQ(nullptr, cbStack3.getActual());
+}
+
+TEST(CallbackStackTest, ClearFromTop) {
+    CallbackStack<ICallback, 2> cbStack2;
+    CallbackStack<ICallback, 3> cbStack3;
+    CBImpl fun1;
+    CBImpl fun2;
+    CBImpl fun3;
+    CBImpl fun4;
+
+    cbStack2.pushBack(&fun1);
+    cbStack2.pushBack(&fun2);
+    cbStack2.remove(&fun2);
+    ASSERT_EQ(&fun1, cbStack2.getActual());
+    cbStack2.remove(&fun1);
+    ASSERT_EQ(nullptr, cbStack2.getActual());
+
+    cbStack3.pushBack(&fun1);
+    cbStack3.pushBack(&fun2);
+    cbStack3.pushBack(&fun3);
+    cbStack3.pushBack(&fun4);
+
+    cbStack3.remove(&fun4);
+    ASSERT_EQ(&fun3, cbStack3.getActual());
+    cbStack3.remove(&fun3);
+    ASSERT_EQ(&fun3, cbStack3.getActual());
+    cbStack3.remove(&fun2);
+    ASSERT_EQ(nullptr, cbStack3.getActual());
+}
+
+TEST(CallbackStackTest, ClearFromBottom) {
+    CallbackStack<ICallback, 2> cbStack2;
+    CallbackStack<ICallback, 3> cbStack3;
+    CBImpl fun1;
+    CBImpl fun2;
+    CBImpl fun3;
+    CBImpl fun4;
+
+    cbStack2.pushBack(&fun1);
+    cbStack2.pushBack(&fun2);
+    cbStack2.remove(&fun1);
+    ASSERT_EQ(&fun2, cbStack2.getActual());
+    cbStack2.remove(&fun2);
+    ASSERT_EQ(nullptr, cbStack2.getActual());
+
+    cbStack3.pushBack(&fun1);
+    cbStack3.pushBack(&fun2);
+    cbStack3.pushBack(&fun3);
+    cbStack3.pushBack(&fun4);
+
+    cbStack3.remove(&fun1);
+    ASSERT_EQ(&fun4, cbStack3.getActual());
+    cbStack3.remove(&fun2);
+    ASSERT_EQ(&fun4, cbStack3.getActual());
+    cbStack3.remove(&fun3);
+    ASSERT_EQ(&fun4, cbStack3.getActual());
+    cbStack3.remove(&fun4);
+    ASSERT_EQ(nullptr, cbStack3.getActual());
+}
