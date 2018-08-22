509c6cbd0ff971374942b9ea2f0f8ee0f2ad2835
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2018-08-22 23:02:41 +0300

    started adding some Unit Tests

--- ClientHelpers/c++/WidgetTopologyContainer.h
+++ ClientHelpers/c++/WidgetTopologyContainer.h
@@ -1,4 +1,4 @@
-template <class DataType, WidgetFunction>
+template <class DataType, class WidgetFunction>
 class WidgetTopologyContainer
 {
 public:
@@ -9,6 +9,14 @@ public:
             m_holder[i] = new DataType[WidgetFunction::getDim[i].x * WidgetFunction::getDim[i].y]
         }
     }
+    ~WidgetTopologyContainer()
+    {
+        for(int i = 0; i <=  WidgetFunction::WidgetId::Last, ++i)
+        {
+            delete m_holder[i];
+        }
+    }
+
     DataType *get(WidgetFunction::WidgetId widgetId, uint8_t x, uint8_t y)
     {
         if(x >= WidgetFunction::getDim[i].x || y >= WidgetFunction::getDim[i].y)
@@ -17,6 +25,17 @@ public:
         }
         return &(m_holder[widgetId][x * WidgetFunction::getDim[i].y + y]);
     }
+
+    template<WidgetFunction::WidgetId WidgetId>
+    DataType *get<WidgetId>(const WidgetTraits<WidgetId>::Coord& coord)
+    {
+        if(x >= WidgetFunction::getDim[i].x || y >= WidgetFunction::getDim[i].y)
+        {
+            return nullptr;
+        }
+        return &(m_holder[widgetId][x * WidgetFunction::getDim[i].y + y]);
+    }
+
 private:
     DataType* m_holder[WidgetFunction::WidgetId::Last+1];
 };
