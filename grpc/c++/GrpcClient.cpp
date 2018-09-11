e07a9624a9bd7c1cf9b19d6811425f98f4e9c17d
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2018-09-11 08:22:35 +0200

    further implementation of grpc client

--- grpc/c++/GrpcClient.cpp
+++ grpc/c++/GrpcClient.cpp
@@ -1,5 +1,152 @@
 #include "GrpcClient.h"
+#include <grpcpp/grpcpp.h>
 
-GrpcClient::GrpcClient(std::shared_ptr<Channel> channel) :
-    m_stub(Greeter::NewStub(channel))
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+
+GrpcClient::GrpcClient(const std::string& serverAddress) :
+    m_stub(FPSimulation::NewStub(grpc::CreateChannel(
+      serverAddress, grpc::InsecureChannelCredentials())))
     {}
+
+bool GrpcClient::led_setColor(const LedSetColorRequest &request)
+{
+    Empty reply;
+    ClientContext context;
+    Status status = m_stub->led_setColor(&context, request, &reply);
+    if (status.ok()) {
+      return true;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return false;
+    }
+}
+
+bool GrpcClient::display_setPixels(const DisplaySetPixelsRequest &request)
+{
+    Empty reply;
+    ClientContext context;
+    Status status = m_stub->display_setPixels(&context, request, &reply);
+    if (status.ok()) {
+      return true;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return false;
+    }
+}
+bool GrpcClient::display_setSubWindowPixels(const DisplaySubWindowPixelsRequest &request)
+{
+    Empty reply;
+    ClientContext context;
+    Status status = m_stub->display_setSubWindowPixels(&context, request, &reply);
+    if (status.ok()) {
+      return true;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return false;
+    }
+}
+bool GrpcClient::display_drawRectangle(const DisplayDrawRectangleRequest &request)
+{
+    Empty reply;
+    ClientContext context;
+    Status status = m_stub->display_drawRectangle(&context, request, &reply);
+    if (status.ok()) {
+      return true;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return false;
+    }
+}
+bool GrpcClient::display_drawLine(const DisplayDrawLineRequest &request)
+{
+    Empty reply;
+    ClientContext context;
+    Status status = m_stub->display_drawLine(&context, request, &reply);
+    if (status.ok()) {
+      return true;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return false;
+    }
+}
+bool GrpcClient::display_setActiveFont(const DisplaySetActiveFontRequest &request)
+{
+    Empty reply;
+    ClientContext context;
+    Status status = m_stub->display_setActiveFont(&context, request, &reply);
+    if (status.ok()) {
+      return true;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return false;
+    }
+}
+bool GrpcClient::display_drawText(const DisplayDrawTextRequest &request)
+{
+    Empty reply;
+    ClientContext context;
+    Status status = m_stub->display_drawText(&context, request, &reply);
+    if (status.ok()) {
+      return true;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return false;
+    }
+}
+bool GrpcClient::display_getResolution(const DisplayResolutionRequest, DisplayResolutionAnswer& answer)
+{
+    return true;
+}
+bool GrpcClient::display_clearDisplay(const DisplayClearDisplayRequest &request)
+{
+    Empty reply;
+    ClientContext context;
+    Status status = m_stub->display_clearDisplay(&context, request, &reply);
+    if (status.ok()) {
+      return true;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return false;
+    }
+}
+bool GrpcClient::getButtonStates(std::vector<GetButtonStateAnswer>& answer)
+{
+    return true;
+}
+bool GrpcClient::getEncoderIncrements(std::vector<GetEncoderIncrementsAnswer>& answer)
+{
+    return true;
+}
+bool GrpcClient::getPotentiometerValues(std::vector<GetPotentiometerValuesAnswer>& answer)
+{
+    return true;
+}
+bool GrpcClient::movePotentiometerToValue(const MovePotentiometerRequest &request)
+{
+    Empty reply;
+    ClientContext context;
+    Status status = m_stub->movePotentiometerToValue(&context, request, &reply);
+    if (status.ok()) {
+      return true;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return false;
+    }
+}
+bool GrpcClient::getTouchValue(std::vector<GetTouchValueAnswer>& answer)
+{
+    return true;
+}
+
--- grpc/c++/GrpcClient.h
+++ grpc/c++/GrpcClient.h
@@ -2,18 +2,30 @@
 #define GRPC_CLIENT_H
 
 #include <memory> // shared_ptr
-#include <grpcpp/grpcpp.h>
-
-using grpc::Channel;
+#include "FPSimulation.grpc.pb.h"
  
 
 class GrpcClient
 {
 public:
-    GrpcClient(std::shared_ptr<Channel> channel);
+    GrpcClient(const std::string& serverAddress);
+    bool led_setColor(const LedSetColorRequest &request);
+    bool display_setPixels(const DisplaySetPixelsRequest &request);
+    bool display_setSubWindowPixels(const DisplaySubWindowPixelsRequest &request);
+    bool display_drawRectangle(const DisplayDrawRectangleRequest &request);
+    bool display_drawLine(const DisplayDrawLineRequest &request);
+    bool display_setActiveFont(const DisplaySetActiveFontRequest &request);
+    bool display_drawText(const DisplayDrawTextRequest &request);
+    bool display_getResolution(const DisplayResolutionRequest, DisplayResolutionAnswer& answer);
+    bool display_clearDisplay(const DisplayClearDisplayRequest &request);
+    bool getButtonStates(std::vector<GetButtonStateAnswer>& answer);
+    bool getEncoderIncrements(std::vector<GetEncoderIncrementsAnswer>& answer);
+    bool getPotentiometerValues(std::vector<GetPotentiometerValuesAnswer>& answer);
+    bool movePotentiometerToValue(const MovePotentiometerRequest &request);
+    bool getTouchValue(std::vector<GetTouchValueAnswer>& answer);
 
 private:
-      std::unique_ptr<Greeter::Stub> m_stub;
+    std::unique_ptr<FPSimulation::Stub> m_stub;
 };
 
 #endif
