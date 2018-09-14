f4819e88797b91c7136b84be421f67d9b93195e6
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2018-09-14 19:52:58 +0200

    renamings

--- FPTopologyExporter.py
+++ FPTopologyExporter.py
@@ -21,7 +21,7 @@ def exportTopology():
                     dim = (int(match.group(2)) + 1,)
 
             if not label in wData:
-                wData[label] = { 'Dimension' : None, 'Function' : None }
+                wData[label] = { 'Dimension' : None, 'wType' : None }
             if(dim and wData[label]['Dimension']):
                 if len(dim) == 1:
                     maxDim = (max(dim[0], wData[label]['Dimension'][0]),)
@@ -30,26 +30,26 @@ def exportTopology():
             else:
                 maxDim = dim
             wData[label]['Dimension'] = maxDim
-            if not wData[label]['Function']:
+            if not wData[label]['wType']:
                 if obj.Name.find("FPSimLinearPotentiometer") == 0:
-                    wData[label]['Function'] = [FPWidgetTypes.POTENTIOMETER, FPWidgetTypes.POT_MOVE]
+                    wData[label]['wType'] = [FPWidgetTypes.POTENTIOMETER, FPWidgetTypes.POT_MOVE]
                 elif obj.Name.find("FPSimButton") == 0:
-                    wData[label]['Function'] = [FPWidgetTypes.BUTTON]
+                    wData[label]['wType'] = [FPWidgetTypes.BUTTON]
                 elif obj.Name.find("FPSimDisplay") == 0:
-                    wData[label]['Function'] = [FPWidgetTypes.DISPLAY]
+                    wData[label]['wType'] = [FPWidgetTypes.DISPLAY]
                 elif obj.Name.find("FPSimRotaryEncoder") == 0:
-                    wData[label]['Function'] = [FPWidgetTypes.ENCODER, FPWidgetTypes.BUTTON]
+                    wData[label]['wType'] = [FPWidgetTypes.ENCODER, FPWidgetTypes.BUTTON]
                 elif obj.Name.find("FPSimRotaryPotentiometer") == 0:
-                    wData[label]['Function'] = [FPWidgetTypes.POTENTIOMETER, FPWidgetTypes.POT_MOVE]
+                    wData[label]['wType'] = [FPWidgetTypes.POTENTIOMETER, FPWidgetTypes.POT_MOVE]
                 elif obj.Name.find("FPSimLED") == 0:
-                    wData[label]['Function'] = [FPWidgetTypes.LED]
+                    wData[label]['wType'] = [FPWidgetTypes.LED]
 
     topology = []
     for label in wData:
-        widget = {'Label' : label, 'Dimension' : wData[label]['Dimension'], 'WidgetFunction' : wData[label]['Function']}
+        widget = {'Label' : label, 'Dimension' : wData[label]['Dimension'], 'WidgetType' : wData[label]['wType']}
         topology.append(widget)
 
     fileName = QtGui.QFileDialog.getSaveFileName(dir = "WidgetTopology.json", filter="WidgetTopology file (WidgetTopology.json)")[0]
     if len(fileName) > 0:
         with open(fileName, 'w') as outfile:
-            json.dump(topology, outfile)
+            json.dump(topology, outfile, indent=3)
