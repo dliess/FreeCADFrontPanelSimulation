f44bca7ae7150d738a2783a16b35fd73896049cf
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2018-09-16 20:36:39 +0200

    try to fix install lib dependency

--- grpc/c++/FpSimGrpcConfig.cmake
+++ grpc/c++/FpSimGrpcConfig.cmake
@@ -1,9 +1,13 @@
 get_filename_component(FpSimGrpc_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(CMakeFindDependencyMacro)
 
-#find_dependency(Boost 1.55 REQUIRED COMPONENTS regex)
-#find_dependency(RapidJSON 1.0 REQUIRED MODULE)
+#find_dependency(grpc REQUIRED MODULE)
+find_dependency(Protobuf CONFIG REQUIRED)
+find_dependency(gRPC CONFIG REQUIRED)
+
 
 if(NOT TARGET FpSimGrpc::FpSimGrpc)
     include("${FpSimGrpc_CMAKE_DIR}/FpSimGrpcTargets.cmake")
 endif()
+
+set_target_properties(FpSimGrpc::FpSimGrpc PROPERTIES IMPORTED_LINK_INTERFACE_LIBRARIES gRpc Protobuf)
