a13c98f432b5ddc310f3334c4850361491d78d24
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2018-09-18 00:30:53 +0200

    fixed grpc install

--- grpc/c++/FpSimGrpcConfig.cmake
+++ grpc/c++/FpSimGrpcConfig.cmake
@@ -1,8 +1,8 @@
 get_filename_component(FpSimGrpc_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(CMakeFindDependencyMacro)
 
-#find_dependency(Boost 1.55 REQUIRED COMPONENTS regex)
-#find_dependency(RapidJSON 1.0 REQUIRED MODULE)
+find_dependency(protobuf REQUIRED CONFIG)
+find_dependency(gRPC  REQUIRED CONFIG)
 
 if(NOT TARGET FpSimGrpc::FpSimGrpc)
     include("${FpSimGrpc_CMAKE_DIR}/FpSimGrpcTargets.cmake")
